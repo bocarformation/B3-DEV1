5a810ed1bcf14f0fc65ccf995a084ddeaf7e211c
bocarformation <bocsco3@gmail.com>
2025-12-16 15:18:06 +0100

    Part 1 of project Stats

--- 01-POO/Exercices/02-Stats/build/index.js
+++ 01-POO/Exercices/02-Stats/build/index.js
@@ -1,3 +1,16 @@
 import fs from "fs";
-const matches = fs.readFileSync;
+const matches = fs.readFileSync("./src/football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((line) => line.split(","));
+console.log(matches);
+let manUnitedWins = 0;
+matches.forEach((match) => {
+    if (match[1] === "Man United" && match[5] === "H") {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === "A") {
+        manUnitedWins++;
+    }
+});
+console.log(`Man United won ${manUnitedWins} games`);
 //# sourceMappingURL=index.js.map
--- 01-POO/Exercices/02-Stats/src/index.ts
+++ 01-POO/Exercices/02-Stats/src/index.ts
@@ -1,3 +1,20 @@
-import fs from "fs"
+import fs from "fs";
 
-const matches = fs.readFileSync
+const matches =
+    fs.readFileSync("./src/football.csv", { encoding: "utf-8" })
+        .split("\n")
+        .map((line: string) => line.split(","))
+console.log(matches);
+
+
+let manUnitedWins = 0;
+matches.forEach((match: string) => {
+    if (match[1] === "Man United" && match[5] === "H") {
+        manUnitedWins++
+    }
+    else if (match[2] === "Man United" && match[5] === "A") {
+        manUnitedWins++
+    }
+})
+
+console.log(`Man United won ${manUnitedWins} games`);
