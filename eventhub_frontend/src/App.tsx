0cafa813e306068c8c2989fe9733795716e72e30
bocarformation <bocsco3@gmail.com>
2025-12-18 18:04:40 +0100

    add final version eventhub front

--- eventhub_frontend/src/App.tsx
+++ eventhub_frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import { Layout } from './modules/app/components/Layout'
+import { AppWrapper } from "./modules/app/components/AppWrapper"
+import { Layout } from "./modules/app/components/Layout"
 
 function App() {
-
-
-  return (
-    <Layout>
-      <h1>Hello World</h1>
-    </Layout>
-  )
-
+	return (
+		<AppWrapper>
+			<Layout>
+				<h1>Hello World</h1>
+			</Layout>
+		</AppWrapper>
+	)
 }
+
 export default App
