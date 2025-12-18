fdea1960f0e36598d99087b20e6a4b1db0c6c98a
bocarformation <bocsco3@gmail.com>
2025-12-18 17:32:27 +0100

    add setup config for event hub

---
+++ eventhub_frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Layout } from './modules/app/components/Layout'
+
+function App() {
+
+
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+    </Layout>
+  )
+
+}
+export default App
